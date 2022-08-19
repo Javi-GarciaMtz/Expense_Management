0c8808297ccd890c5847ce37cc8a2b8804cac1d1
Javi-GarciaMtz <javi.javier.gm@gmail.com>
2022-08-18 23:42:53 -0500

    insert expense is working

--- test.php
+++ test.php
@@ -23,5 +23,20 @@ if ($result = $mysqli->query("SELECT * FROM expenses")) {
     // $result->close();
 }
 
+$name_expense = "prueba de gasto desde php 2";
+$cost = 23.23;
+$date = date("Y-m-d");
+$description = "esto es una descriopcion desde php";
+
+$query_insert = "INSERT INTO expenses (id, name, cost, date, description) VALUES (NULL, '".$name_expense."' , '".$cost."' , '".$date."' , '".$description."' )";
+
+// var_dump($query_insert); die();
+
+if ($result = $mysqli->query($query_insert)) {
+
+    var_dump($result);
+
+}
+
 
 
